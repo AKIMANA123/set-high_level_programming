af7599c41dad65a3b674bb868d31d9933e655208
AKIMANA123 <axelakimana78@gmail.com>
2026-08-25 20:01:34 +0200

    Refactor State class and relationship definition

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""State class definition with relationship to City using SQLAlchemy"""
-
+"""Contains class definition of State with relationship to City."""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State class that links to the MySQL table states"""
-    
+    """State class with relationship to City."""
     __tablename__ = 'states'
-    
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    
-    # Relationship with City class
-    # - backref="state" creates a 'state' attribute in City objects
-    # - cascade="all, delete-orphan" ensures cities are deleted when state is deleted
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship(
+        "City",
+        backref="state",
+        cascade="all, delete-orphan"
+    )
