05e53fcfbbb53dde38169b558bd133e5c06732f1
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 09:08:17 -0700

    Add 2-post_email.py to send POST request with email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Sends a POST request to a URL with an email parameter using urllib."""
-import sys
-import urllib.parse
+"""Sends a POST request with email parameter and displays response body"""
+
 import urllib.request
+import urllib.parse
+import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
+
+    # Encode the email parameter
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data)
+
+    # Create a POST request
+    req = urllib.request.Request(url, data=data, method='POST')
+
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+        # Read and decode the response body
+        content = response.read().decode('utf-8')
+        print(content)
