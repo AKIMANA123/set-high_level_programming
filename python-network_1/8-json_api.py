eba260303cf7da4adef6e65f29d2b0a819642fe9
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 09:32:18 -0700

    Add 8-json_api.py to search user with JSON response

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""Sends a POST request to search_user with a letter parameter."""
-import sys
+"""Sends a POST request with a letter and handles JSON response"""
+
 import requests
+import sys
+
 
 if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
-    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    # Get the letter from command line or set to empty string
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    # Send POST request with q parameter
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+    response = requests.post(url, data=data)
+
     try:
-        data = r.json()
-        if data:
-            print("[{}] {}".format(data.get("id"), data.get("name")))
+        json_response = response.json()
+        if json_response:
+            id = json_response.get('id')
+            name = json_response.get('name')
+            print("[{}] {}".format(id, name))
         else:
             print("No result")
     except ValueError:
