261573a32f2fdbc35e915e590a41be63956fb1e9
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 08:44:14 -0700

    Fix 0-hbtn_status.py to only import urllib

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,17 +2,11 @@
 """Fetches https://intranet.hbtn.io/status using urllib"""
 
 import urllib.request
-import ssl
 
 
 if __name__ == "__main__":
-    # Create SSL context that doesn't verify certificates (for testing)
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-
     url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url, context=ssl_context) as response:
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
