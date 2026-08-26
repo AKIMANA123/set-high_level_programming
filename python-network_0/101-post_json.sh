f508c52ec04ba6537694a4f65dd1e106a8fc5e8e
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 07:25:15 -0700

    Fix 101-post_json.sh to be 3 lines long

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Script that sends a JSON POST request with the contents of a file
-
-if ! jq . "$2" > /dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 0
-fi
-
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+jq . "$2" > /dev/null 2>&1 && curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" || echo "Not a valid JSON"
