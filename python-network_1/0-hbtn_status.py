af27b80f77e27b922626d4c617d83305337c6496
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 08:34:11 -0700

    Add 0-hbtn_status.py to fetch status using urllib

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://alx-intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status using urllib"""
+
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
