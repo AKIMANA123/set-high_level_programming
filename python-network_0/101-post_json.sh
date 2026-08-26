e9929d18881c8194db5e9491bdeb036862f0fac0
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 07:30:11 -0700

    Fix 101-post_json.sh to let server validate JSON

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request with contents of a file and displays response
-jq . "$2" > /dev/null 2>&1 && curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" || echo "Not a valid JSON"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
