1fa989ce3a3c0d0c3c6f502d209f8f6cf70276b1
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 16:46:39 +0200

    Modify catch_me.sh to improve request format
    Updated the curl command to include data and change headers.

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!"
-curl -s -X PUT -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me
+# Makes a request to cause response body to contain 'You got me!'
+curl -sL -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
