82b0352f1b435d9725c126cfa86a7d3dea07a5ee
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 09:26:07 -0700

    Fix pycodestyle in 6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request with email parameter and displays response body using requests"""
+"""Sends a POST request with email param and displays response body"""
 
 import requests
 import sys
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
+
     data = {'email': email}
     response = requests.post(url, data=data)
     print(response.text)
