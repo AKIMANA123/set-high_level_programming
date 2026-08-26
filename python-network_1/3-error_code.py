091dbd50342cd912de154f8a2486699091b9e298
AKIMANA123 <axelakimana78@gmail.com>
2026-08-26 09:10:54 -0700

    Add 3-error_code.py to handle HTTP errors

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Sends a request to a URL and handles urllib HTTP error exceptions."""
-import sys
-import urllib.error
+"""Sends a request to URL and displays response body, handling HTTP errors"""
+
 import urllib.request
+import urllib.error
+import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
+    
     try:
-        with urllib.request.urlopen(req) as response:
-            print(response.read().decode('utf-8'))
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
